7cb33790eeb63d66384dd606f26776efe0447b99
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-02-27 16:00:53 -0400

    Se eliminaron archivos predeterminados

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { nanoid } from 'nanoid';
 
 function App() {
+
+
   return (
     <div className="App">
-      
+      Hello
     </div>
   );
 }
