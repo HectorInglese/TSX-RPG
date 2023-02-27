fc287ac5853d5ee47896071a9c56d424107232dd
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-02-27 15:05:14 -0400

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { nanoid } from 'nanoid';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
