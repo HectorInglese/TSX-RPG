14d77b0be1699b6acc9ab1b9d5f782c5ecc78d90
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-03-06 08:48:02 -0400

    HeroText created

--- src/Components/HeroText.tsx
+++ src/Components/HeroText.tsx
@@ -1,3 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 
+const HeroText : React.FC = () => {
+
+    return (
+        <div className="heroText">
+            Hello text
+        </div>
+    )
+}
+
+
+export default HeroText;
