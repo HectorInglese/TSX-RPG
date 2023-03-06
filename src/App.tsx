d5e470d419ba9efa1f380d606f6cfc634dd9c0e8
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-03-06 16:54:46 -0400

    Solve type problem in toggle, it gives id

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import React, { FC , useState } from 'react';
+import React, { FC, useState } from "react";
 
-import boxprop from './json/boxes';
-import BoxProps from './Interfaces/boxInterface';
+import boxprop from "./json/boxes";
+import BoxProps from "./Interfaces/boxInterface";
 
-import Boxes from './Components/Boxes';
+import Boxes from "./Components/Boxes";
 
-import "./App.css"
+import "./App.css";
 
 function App() {
+  const [square, setSquare] = useState();
 
-  const [square, setSquare] = useState()
+  const toggle = ( id : number) : boolean => {
+    console.log("Clicked " + id);
 
-  const toggle = ({id} : BoxProps) =>{
-    console.log("Clicked " + id)
-    return true
-  }
+    return true;
+  };
 
-  const squares = boxprop.map(square => (
-    <Boxes 
-      key={square.id}
-      id={square.id}
-      on={square.on}
-      toggle = {toggle}
-    />
-  ))
-  return (
-    <div className="App">
-      {squares}
-    </div>
-  );
+  const squares = boxprop.map((square) => (
+    <Boxes key={square.id} id={square.id} on={square.on} toggle={toggle} />
+  ));
+  return <div className="App">{squares}</div>;
 }
 
 export default App;
